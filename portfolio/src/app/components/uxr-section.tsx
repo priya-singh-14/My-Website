6e90c56705f7b6fbc49f6143014983fda2d7272c
priya-singh-14 <singh.priya3@northeastern.edu>
2025-08-03 17:34:41 -0400

    feat; uxr section for case studies

---
+++ portfolio/src/app/components/uxr-section.tsx
@@ -0,0 +1,66 @@
+"use client";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+import { UXRSection } from "../utils/types";
+
+interface UXRSectionProps {
+  sectionDetails: UXRSection;
+}
+
+export default function UXRSection({ sectionDetails }: UXRSectionProps) {
+  const ImageCarousel = () => {
+    const images = sectionDetails.carousel;
+
+    const responsive = {
+      desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 1,
+        partialVisibilityGutter: 40,
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 640 },
+        items: 1,
+        partialVisibilityGutter: 30,
+      },
+      mobile: {
+        breakpoint: { max: 640, min: 0 },
+        items: 1,
+        partialVisibilityGutter: 20,
+      },
+    };
+
+    return (
+      <div className="w-full carousel-wrapper">
+        <Carousel
+          responsive={responsive}
+          arrows={true}
+          infinite={true}
+          autoPlay={false}
+          keyBoardControl={true}
+          containerClass="carousel-container"
+          itemClass="carousel-item"
+        >
+          {images.map((img, index) => (
+            <div key={index}>
+              <img
+                src={img}
+                className="pb-5 w-full h-auto object-contain max-h-[800px] md:max-h-[600px] sm:max-h-[400px]"
+              />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    );
+  };
+
+  return (
+    <div className="w-full h-full p-10">
+      <h4 className="text-h4 font-mono uppercase px-5 pb-10 mr-20 text-blackPrimary">
+        User Research
+      </h4>
+      <ImageCarousel></ImageCarousel>
+      <div className="flex pt-10"></div>
+    </div>
+  );
+}
