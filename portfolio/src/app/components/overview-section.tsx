1c0bf9609066a3cc5dfdccdee205fb32fe7a4a81
priya-singh-14 <singh.priya3@northeastern.edu>
2025-08-03 16:08:45 -0400

    feat: overview section component for case studies

---
+++ portfolio/src/app/components/overview-section.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { OverviewSection } from "../utils/types";
+
+interface OverviewSectionProps {
+  sectionDetails: OverviewSection;
+}
+
+export default function OverviewSection({
+  sectionDetails,
+}: OverviewSectionProps) {
+  return (
+    <div className="flex w-full h-full p-10">
+      <div className="w-1/2 px-5 mr-20">
+        <h4 className="font-mono text-h4 text-blackPrimary uppercase">
+          The Problem
+        </h4>
+        <p className="pt-5 text-p2 text-blackPrimary">
+          {sectionDetails.problem}
+        </p>
+      </div>
+      <div className="w-1/2 px-5 mr-20">
+        <h4 className="font-mono text-h4 text-blackPrimary uppercase">
+          User Research
+        </h4>
+        <p className="pt-5 text-p2 text-blackPrimary">
+          {sectionDetails.uxr.split("/").map((section, index) => (
+            <p key={index} className="pb-4">
+              {section}{" "}
+            </p>
+          ))}
+        </p>
+      </div>
+    </div>
+  );
+}
