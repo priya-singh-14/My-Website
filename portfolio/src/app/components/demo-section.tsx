e6535fb1776cfaa6563f054c0b251f984602f557
priya-singh-14 <singh.priya3@northeastern.edu>
2025-08-03 17:54:59 -0400

    feat: demo section for case studies

---
+++ portfolio/src/app/components/demo-section.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { DemoSection } from "../utils/types";
+
+interface DemoSectionProps {
+  sectionDetails: DemoSection;
+}
+
+export default function DemoSection({ sectionDetails }: DemoSectionProps) {
+  const Video = () => {
+    const video = sectionDetails.demo;
+
+    return (
+      <div className="mt-4 w-full md:mt-0 md:w-3/4 m-auto">
+        <video
+          className="overflow-hidden object-cover"
+          src={video}
+          autoPlay
+          loop
+          muted
+          playsInline
+        />
+      </div>
+    );
+  };
+
+  return (
+    <div className="w-full h-full p-10">
+      <h4 className="text-h4 font-mono uppercase px-5 mr-20 pb-10 text-blackPrimary">
+        Final Solution
+      </h4>
+      <Video></Video>
+    </div>
+  );
+}
