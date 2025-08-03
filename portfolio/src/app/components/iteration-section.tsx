4607a13f4ea175718ff2bde96adf89a57d7f301a
priya-singh-14 <singh.priya3@northeastern.edu>
2025-08-03 17:50:34 -0400

    feat: iteration section for case studies

---
+++ portfolio/src/app/components/iteration-section.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { IterationSection } from "../utils/types";
+
+interface IterationSectionProps {
+  sectionDetails: IterationSection;
+}
+
+export default function IterationSection({
+  sectionDetails,
+}: IterationSectionProps) {
+  return (
+    <div className="w-full h-full p-10">
+      <h4 className="text-h4 font-mono uppercase px-5 mr-20 text-blackPrimary">
+        Iteration
+      </h4>
+      <div className="flex gap-x-4 mt-5 mx-5">
+        <img
+          className="w-2/3 border border-greyPrimary object-cover"
+          src={sectionDetails.wireframes}
+        ></img>
+        <div className="w-1/3 flex flex-wrap gap-y-4">
+          <img
+            className="w-full border border-greyPrimary object-cover"
+            src={sectionDetails.system}
+          ></img>
+          <img
+            className="w-full border border-greyPrimary object-cover"
+            src={sectionDetails.microint}
+          ></img>
+        </div>
+      </div>
+      <img
+        className="w-full mt-5 mx-5 border border-greyPrimary object-cover"
+        src={sectionDetails.colortype}
+      ></img>
+    </div>
+  );
+}
