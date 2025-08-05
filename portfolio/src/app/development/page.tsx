230fec11bc48b7cd0b55076556e404182af7eafe
priya-singh-14 <singh.priya3@northeastern.edu>
2025-08-05 13:07:41 -0400

    fix: removing unused pages

--- portfolio/src/app/development/page.tsx
+++
@@ -1,8 +0,0 @@
-import PageLayout from "../components/page-layout";
-import { devProjects } from "../utils/projects";
-
-export default function Development() {
-  return (
-    <PageLayout title={"DEVELOPMENT"} description={"With a background in Computer Science and Design, I do my best work where creativity meets logic. My development work focuses on connecting intuitive user interfaces to thoughtful, functional experiences— all brought to life through code."} canvasImage={"bw.png"} projects={devProjects}></PageLayout>
-  );
-}
