85341946328c013e77a7a5803f361af3029d438c
priya-singh-14 <singh.priya3@northeastern.edu>
2025-08-06 10:24:09 -0400

    feat: responsive header section

--- portfolio/src/app/components/header-section.tsx
+++ portfolio/src/app/components/header-section.tsx
@@ -16,22 +16,25 @@ export default function HeaderSection({ sectionDetails }: HeaderSectionProps) {
   };
 
   return (
-    <div className="py-20 flex w-full h-full border border-blackPrimary border-opacity-25 p-10">
-      <div className="w-1/2 pr-20">
+    <div className="p-5 py-10 md:py-20 flex flex-wrap w-full h-full border border-blackPrimary border-opacity-25">
+      <div className="md:hidden w-full h-auto mb-10">
+        <img src={sectionDetails.mockups} className="object-cover"></img>
+      </div>
+      <div className="w-full md:w-1/2 md:pr-20">
         <h3 className="font-mono text-h3 px-5 pb-3 text-blackPrimary uppercase">
           {sectionDetails.title}
         </h3>
         <p className="font-mono font-light px-5 text-greyPrimary uppercase">
           {sectionDetails.subtitle}
         </p>
-        <p className="font-sans text-p2 p-5 pb-10 text-blackPrimary">
+        <p className="font-sans text-p2 p-5 md:pb-10 text-blackPrimary">
           {sectionDetails.description}
         </p>
         <div className="w-1/2 m-5 w-full flex flex-wrap gap-2">
           {sectionDetails.tags.map((tag, i) => {
             return (
               <div key={i} className="bottom-0 border border-greyPrimary">
-                <p className="text-sm text-nowrap px-3 py-1 font-mono uppercase text-greyPrimary">
+                <p className="text-xs md:text-sm text-nowrap px-3 py-1 font-mono uppercase text-greyPrimary">
                   {tag}
                 </p>
               </div>
@@ -45,7 +48,7 @@ export default function HeaderSection({ sectionDetails }: HeaderSectionProps) {
                 key={i}
                 className="bottom-0 bg-greyLight bg-opacity-25 border border-greyLight"
               >
-                <p className="text-sm text-nowrap px-3 py-1 font-mono uppercase text-greyPrimary">
+                <p className="text-xs md:text-sm text-nowrap px-3 py-1 font-mono uppercase text-greyPrimary">
                   {tag}
                 </p>
               </div>
@@ -53,13 +56,13 @@ export default function HeaderSection({ sectionDetails }: HeaderSectionProps) {
           })}{" "}
         </div>
         <button onClick={scrollToTarget}>
-          <p className="uppercase underline font-mono font-light pt-20 px-5 hover:text-bluePrimary">
+          <p className="uppercase underline font-mono font-light pt-10 md:pt-20 px-5 hover:text-bluePrimary">
             View final solution
           </p>
         </button>
       </div>
-      <div className="w-1/2">
-        <img src={sectionDetails.mockups} className="object-cover m-auto"></img>
+      <div className="hidden md:block md:w-1/2">
+        <img src={sectionDetails.mockups} className="object-cover"></img>
       </div>
     </div>
   );
