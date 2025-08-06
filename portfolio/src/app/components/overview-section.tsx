6aea7c976a8c474b01f4f0ef5d50a071c9a57aef
priya-singh-14 <singh.priya3@northeastern.edu>
2025-08-06 10:53:52 -0400

    feat: responsive overview

--- portfolio/src/app/components/overview-section.tsx
+++ portfolio/src/app/components/overview-section.tsx
@@ -9,12 +9,12 @@ export default function OverviewSection({
   sectionDetails,
 }: OverviewSectionProps) {
   return (
-    <div className="flex w-full h-full p-10">
-      <div className="w-1/2 px-5 mr-20">
+    <div className="w-full h-full flex flex-wrap p-5 mt-10 md:flex-nowrap md:p-10">
+      <div className="w-full p-4 mb-8 md:w-1/2 md:px-5 md:mr-20">
         <h4 className="font-mono text-h4 text-blackPrimary uppercase">
           The Problem
         </h4>
-        <div className="pt-5 font-sans text-p text-blackPrimary">
+        <div className="pt-5 font-sans text-p2 text-blackPrimary md:pb-0 md:text-p">
           {sectionDetails.problem.split("/").map((section, index) => (
             <p key={index} className="pb-4">
               {section}{" "}
@@ -22,11 +22,11 @@ export default function OverviewSection({
           ))}
         </div>
       </div>
-      <div className="w-1/2 px-5 mr-20">
+      <div className="w-full p-4 mb-8 md:w-1/2 md:px-5 md:mr-20">
         <h4 className="font-mono text-h4 text-blackPrimary uppercase">
           The Solution
         </h4>
-        <div className="pt-5 font-sans text-p text-blackPrimary">
+        <div className="pt-5 font-sans text-p2 md:text-p text-blackPrimary">
           {sectionDetails.uxr.split("/").map((section, index) => (
             <p key={index} className="pb-4">
               {section}{" "}
