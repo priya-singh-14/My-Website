9ae80aef51d596ef5c355561eacabf7a2b0ac48b
priya-singh-14 <singh.priya3@northeastern.edu>
2025-08-11 18:52:53 -0400

    feat: external links

--- portfolio/src/app/components/all-projects-layout.tsx
+++ portfolio/src/app/components/all-projects-layout.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import NewProjectCard from "./new-project-card";
 import { NewProject } from "../utils/types";
+import Link from "next/link";
 
 interface AllProjectsProps {
   title: string;
@@ -14,16 +15,19 @@ export default function AllProjectsLayout(props: AllProjectsProps) {
   const [viewedProjects, setViewedProjects] = useState("All");
 
   function filterProjects(): NewProject[] {
-    let filteredProjects : NewProject[] = []
-    
+    let filteredProjects: NewProject[] = [];
+
     props.projects.forEach((project) => {
-        if (project.subtitle.includes(viewedProjects) || viewedProjects == "All") {
-            filteredProjects.push(project);
-        }
+      if (
+        project.subtitle.includes(viewedProjects) ||
+        viewedProjects == "All"
+      ) {
+        filteredProjects.push(project);
+      }
     });
 
     return filteredProjects;
-}
+  }
 
   return (
     <div className="w-full pb-10">
@@ -34,32 +38,53 @@ export default function AllProjectsLayout(props: AllProjectsProps) {
         {props.description}
       </h4>
       <hr className="mx-5 md:mx-10"></hr>
-      <div className="flex flex-wrap gap-2 mx-5 md:mx-10 mt-5 font-sans">
-        <button
-          onClick={() => setViewedProjects("All")}
-          className={`text-greyPrimary ${
-            viewedProjects == "All" ? "font-semibold" : "font-light"
-          }`}
-        >
-          ALL |
-        </button>
-        <button
-          onClick={() => setViewedProjects("Dev")}
-          className={`text-greyPrimary ${
-            viewedProjects == "Dev" ? "font-semibold" : "font-light"
-          }`}
-        >
-          DEV |
-        </button>
-        <button
-          onClick={() => setViewedProjects("Design")}
-          className={`text-greyPrimary ${
-            viewedProjects == "Design" ? "font-semibold" : "font-light"
-          }`}
-        >
-          DESIGN
-        </button>
+      <div className="flex flex-wrap items-center justify-between mx-5 md:mx-10 mt-5 font-sans">
+        <div className="flex gap-2">
+          <button
+            onClick={() => setViewedProjects("All")}
+            className={`text-greyPrimary ${
+              viewedProjects == "All" ? "font-semibold" : "font-light"
+            }`}
+          >
+            ALL |
+          </button>
+          <button
+            onClick={() => setViewedProjects("Dev")}
+            className={`text-greyPrimary ${
+              viewedProjects == "Dev" ? "font-semibold" : "font-light"
+            }`}
+          >
+            DEV |
+          </button>
+          <button
+            onClick={() => setViewedProjects("Design")}
+            className={`text-greyPrimary ${
+              viewedProjects == "Design" ? "font-semibold" : "font-light"
+            }`}
+          >
+            DESIGN
+          </button>
+        </div>
+        <div className="flex gap-4">
+          <Link
+            href="https://github.com/priya-singh-14"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="uppercase hover:none md:hover:underline font-sans font-light text-greyPrimary"
+          >
+            Github
+          </Link>
+          <Link
+            href="https://linkedin.com/in/priyagracesingh"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="uppercase hover:none md:hover:underline font-sans font-light text-greyPrimary"
+          >
+            LinkedIn
+          </Link>
+        </div>
       </div>
+
       <div className="m-auto w-full px-5 pt-10 gap-y-10 grid grid-cols-1 md:px-10">
         {filterProjects().map((project, index) => (
           <NewProjectCard key={index} project={project} />
